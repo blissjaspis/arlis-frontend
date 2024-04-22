0903896f4265e7e427af8ef715a2e8e19d1067f0
Bliss Jaspis <jaspis_zzt@ymail.com>
2024-04-22 15:09:26 +0700

    add service

---
+++ src/components/ServiceDialogCreate.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+</script>
+<template>
+    <Dialog>
+        <DialogTrigger>
+            <Button size="sm">Create New Service</Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Form Create Service</DialogTitle>
+                <DialogDescription>
+                    Create a service that you want.
+                </DialogDescription>
+            </DialogHeader>
+
+            <div class="flex flex-col gap-5">
+                <Input placeholder="Name" />
+
+                <Input type="number" placeholder="Price" />
+
+                <Select>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Select type service" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Types</SelectLabel>
+                            <SelectItem value="gold">
+                                Gold
+                            </SelectItem>
+                            <SelectItem value="bronze">
+                                Bronze
+                            </SelectItem>
+                            <SelectItem value="silver">
+                                Silver
+                            </SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
+                <Input type="number" placeholder="Duration Months" />
+            </div>
+
+            <DialogFooter>
+                <Button>Save Service</Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+</template>
---
+++ src/views/ServiceView.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table'
+import ServiceDialogCreate from '@/components/ServiceDialogCreate.vue';
+import { Button } from '@/components/ui/button'
+
+</script>
+<template>
+    <div class="container mx-auto my-20">
+        <Card>
+            <CardHeader>
+                <CardTitle>Service</CardTitle>
+                <CardDescription>All services</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ServiceDialogCreate />
+
+                <Table class="mt-10">
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>
+                                Name
+                            </TableHead>
+                            <TableHead>Price</TableHead>
+                            <TableHead>Type</TableHead>
+                            <TableHead class="text-right">
+                                Duration
+                            </TableHead>
+                            <TableHead class="text-right"></TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell class="font-medium">
+                                Gyms Gold
+                            </TableCell>
+                            <TableCell>Rp 1.000.000</TableCell>
+                            <TableCell>Gold</TableCell>
+                            <TableCell class="text-right">
+                                1 Month
+                            </TableCell>
+                            <TableCell class="text-right">
+                                <div class="flex justify-end gap-2">
+                                    <Button variant="secondary">Edit</Button>
+                                    <Button variant="destructive">Delete</Button>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
+    </div>
+</template>
