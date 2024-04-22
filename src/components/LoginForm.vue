7f2eceae7cc148c32928b91939fba233a9ef251a
Bliss Jaspis <jaspis_zzt@ymail.com>
2024-04-22 14:15:32 +0700

    add login view and remove unused views

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+</script>
+<template>
+    <Card>
+        <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Login to AR GYM</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <Input type="email" placeholder="Email" class="mb-5" />
+            <Input type="password" placeholder="Password" class="mb-5" />
+            <Button>Login</Button>
+        </CardContent>
+    </Card>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LoginForm from '../components/LoginForm.vue'
+</script>
+
+<template>
+    <main class="max-w-lg mx-auto my-20">
+        <LoginForm />
+    </main>
+</template>
